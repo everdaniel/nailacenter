bff70f7de90f64ef573dec771a96e32d9e6a7979
Ever Daniel Barreto <everdaniel@gmail.com>
2013-04-15 09:41:49 -0400

    Add project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nailacenter::Application.config.secret_token = 'f26ef9f82ef4de2cb6227262f0a50ddddbff1b66a52e606aa3250aa3c3a90865ad5ca410b163027aeb9f2558d7c590047e12265412f036566e4c052db8378a78'
